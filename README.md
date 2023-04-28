9d30d95841f5ba4bf540bceb116a9afc12f04a22
Tanran <taz609@g.harvard.edu>
2023-04-28 15:46:24 -0400

    UPDATE: logger, predict

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # rbda-ml-inference
 
 Code to run inference of rbda project, NYU
+
+To build venv (for the first time) on MacOS, run:
+```
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+To activate venv, run:
+```
+source venv/bin/activate
+```
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+import argparse
 from detoxify import Detoxify
+from logger import create_logger
+import torch
+import pandas as pd
 
-results = Detoxify('original', device='cuda').predict('example text')
-print(results)
+parser = argparse.ArgumentParser(description='Inference')
+
+
+
+def main():
+    logger = create_logger("inference.log")
+    if torch.cuda.is_available():
+        device = torch.device('cuda') 
+    else:
+        device = torch.device('cpu')
+    logger.info(f'Using device: {str(device).upper()}.\n')
+    
+    model = Detoxify('original', device=device)
+    test_text = [
+        "<p>the solution can be obtained by using the <strong>outputstream</strong> and using <strong>files.",
+        "Hey fk you",
+        "<p>Dumbass, the solution can be obtained by using the <strong>outputstream</strong> and using <strong>files.",
+    ]
+    result = model.predict(test_text)
+    print(pd.DataFrame(result, index=test_text).round(5))
+
+if __name__ == "__main__":
+    main()
