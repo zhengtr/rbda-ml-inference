8ea89b285750692ea21a0d93914c01dec80ea9aa
Tanran <taz609@g.harvard.edu>
2023-04-30 13:44:06 -0400

    add: reddit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Inference')
-parser.add_argument("--data_type", required=True, choices=['questions', 'answers'], type=str, help="Data Type")
+parser.add_argument("--data_type", required=True, choices=['questions', 'answers', 'reddit'], type=str, help="Data Type")
 parser.add_argument("--data_path", default="./full_data",type=str, help="Path to data file")
 parser.add_argument("--chunk_size", default=100, type=int, help="Inference batch size")
 
@@ -22,15 +22,17 @@ def main():
         device = torch.device('cpu')
     logger.info(f'Using device: {str(device).upper()}.')
 
-    
     input_file = args.data_path + f"/{args.data_type}_part-r-00000.csv"
     output_file = args.data_path + f"/{args.data_type}_score.csv"
 
     # dataset
     if args.data_type == "questions":
         header = ["Source", "YearOfCreation", "Id", "Score", "CW_count", "Body"]
-    else:
+    elif args.data_type == "answers":
         header = ["Source", "YearOfCreation", "ParentId", "Score", "CW_count","Body"]
+    elif args.data_type == "reddit":
+        header = ["Source","Body"]
+
     my_dataset = CSVDataset(path=input_file, header=header, chunk_size=args.chunk_size)
     data_loader = torch.utils.data.DataLoader(my_dataset, collate_fn=my_collate, batch_size=1, shuffle=False)
     
