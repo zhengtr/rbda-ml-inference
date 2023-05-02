e88d3e64a053df9abf20e9c261ae109bfa156621
zhengtr <zhengtr@gmail.com>
2023-05-02 02:15:58 -0400

    partition

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description='Inference')
 parser.add_argument("--data_type", required=True, choices=['questions', 'answers', 'reddit', 'inst'], type=str, help="Data Type")
 parser.add_argument("--data_path", default="./full_data",type=str, help="Path to data file")
 parser.add_argument("--chunk_size", default=100, type=int, help="Inference batch size")
-
+parser.add_argument("--partition", default=100000, type=int, help="Inference batch size")
 
 def main():
     args = parser.parse_args()
@@ -24,7 +24,7 @@ def main():
 
     input_file = args.data_path + f"/{args.data_type}_part-r-00000.csv"
     # input_file = args.data_path + f"/mini_{args.data_type}.csv"
-    output_file = args.data_path + f"/{args.data_type}_score.csv"
+    output_file = args.data_path + f"/{args.data_type}_score"
 
     # dataset
     if args.data_type == "questions":
@@ -38,18 +38,20 @@ def main():
     data_loader = torch.utils.data.DataLoader(my_dataset, collate_fn=lambda x: x[0], batch_size=1, shuffle=False)
     
     model = Detoxify('original', device=device)
-    use_header, invalid_count = True, 0
+    use_header, invalid_count, total_count = True, 0, 0
     pbar = tqdm(data_loader, desc="Scoring")
-    with open(output_file, 'a', newline='') as fp:
-        for key, body in pbar:
-            try:
-                res = model.predict(body)
-                pd.DataFrame(res, index=key).round(5).rename_axis('Source').reset_index().to_csv(fp, index=False, header=use_header)
-            except Exception:
-                invalid_count += 1
-                continue
-            use_header = False
-            pbar.set_postfix({'Invalid': {f"{invalid_count}"}})
+    for key, body in pbar:
+        try:
+            res = model.predict(body)
+            pd.DataFrame(res, index=key).round(5).rename_axis('Source').reset_index().to_csv(output_file+".csv", mode='a', index=False, header=use_header)
+        except Exception:
+            invalid_count += 1
+            continue
+        total_count += 1
+        if total_count % args.partition == 0:
+            output_file += '_p'
+        use_header = False
+        pbar.set_postfix({'Invalid': {f"{invalid_count}"}})
 
 if __name__ == "__main__":
     main()
